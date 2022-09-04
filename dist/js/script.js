82a766d2ad03e44312e1839d4dfecbb7da04f29a
JusmanEfendy <njhussrhiee0206@gmail.com>
2022-09-04 00:11:26 -0500

    darkmode update

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
+const hamberger = document.querySelector('#hamberger')
+const navMenu = document.querySelector('#menu')
+const darkMode = document.querySelector('#darkmenu')
+const html = document.querySelector('html')
+
+
 window.onscroll = function () {
   const header = document.querySelector('.header')
   const fixed = header.offsetTop
@@ -12,15 +18,10 @@ window.onscroll = function () {
   }
 }
 
-
-const hamberger = document.querySelector('#hamberger')
-const navMenu = document.querySelector('#menu')
-
 hamberger.addEventListener('click', function() {
   hamberger.classList.toggle('hamberger-active')
   navMenu.classList.toggle('hidden')
 })
-
 
 window.addEventListener('click', function (e) {
   if (e.target != hamberger && e.target != navMenu) {
@@ -29,11 +30,13 @@ window.addEventListener('click', function (e) {
   }
 })
 
-
-// darkmode 
-const darkMode = document.querySelector('#darkmenu')
-const html = document.querySelector('html')
-
 darkMode.addEventListener('click', function () {
   darkMode.checked ? html.classList.add('dark') : html.classList.remove('dark')
+  darkMode.checked ? localStorage.theme = 'dark' : localStorage.theme = 'light'
 })
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkMode.checked = true
+} else {
+  darkMode.checked = false
+}
